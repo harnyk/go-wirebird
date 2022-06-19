7ff35a9827c09c8de30fee77b7618f36934977e3
Mark <markh@sweepbright.com>
2022-06-19 13:27:51 +0300

    wip

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,28 +1,42 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/danielgtaylor/huma"
 	"github.com/danielgtaylor/huma/cli"
 	"github.com/danielgtaylor/huma/responses"
+	"github.com/harnyk/go-wirebird/internal/models"
 )
 
 type LoggerResult struct {
 	Accepted bool `json:"accepted"`
 }
 
+type V1LoggerEventInput struct {
+	Body models.LoggerEvent `json:"body"`
+}
+
 func main() {
 	app := cli.NewRouter("Wirebird API", "0.0.1")
-	// app.DocsHandler(huma.SwaggerUIHandler(app.Router))
+	app.DocsHandler(huma.SwaggerUIHandler(app.Router))
 
-	app.Resource("/").Post("post-logger-event", "Save a logger event",
-		responses.OK().Model(&LoggerResult{}),
-	).Run(func(ctx huma.Context) {
-		ctx.Header().Set("Content-Type", "application/json")
-		ctx.WriteModel(http.StatusOK, &LoggerResult{Accepted: true})
-	})
-
+	app.Resource("/updates").
+		Post(
+			"post-logger-event-v1",
+			"Save a logger event (backwards compatible)",
+			responses.
+				OK().
+				Model(&LoggerResult{}),
+		).
+		Run(
+			func(ctx huma.Context, input V1LoggerEventInput) {
+				ctx.Header().Set("Content-Type", "application/json")
+				log.Printf("Received event: %+v", input)
+				ctx.WriteModel(http.StatusOK, &LoggerResult{Accepted: true})
+			},
+		)
 	app.Run()
 
 }
